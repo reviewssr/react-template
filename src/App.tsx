c9fd232b63d06008af36bcd68ca4aec217f884f9
reviewssr <hangview@outlook.com>
2022-08-24 17:05:02 +0800

    feat: layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,39 @@
 import './App.less';
-import { Button } from 'antd';
+import { Button, Layout, Card } from 'antd';
 import styled from '@emotion/styled';
+const { Header, Footer, Content } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <EmotionButton type="primary">antd button</EmotionButton>
+      <Layout>
+        <HeaderArea>react-template</HeaderArea>
+        <ContentArea>
+          <SmallCard size="small"> some texts ...</SmallCard>
+        </ContentArea>
+        <FootArea>@reviewssr</FootArea>
+      </Layout>
     </div>
   );
 }
 
 export default App;
-
-const EmotionButton = styled(Button)`
-  margin: 20rem;
+const HeaderArea = styled(Header)`
+  height: 5rem;
+  color: white;
+  display: flex;
+  align-items: center;
+`;
+const ContentArea = styled(Content)`
+  height: calc(100vh - 10rem);
+  padding: 2rem;
 `;
 
-// test format
-const foo = () => {
-  for (let i = 0; i < 10; i++) console.log('Hello!');
-};
+const FootArea = styled(Footer)`
+  height: 5rem;
+  text-align: center;
+`;
+
+const SmallCard = styled(Card)`
+  width: 20rem;
+`;
